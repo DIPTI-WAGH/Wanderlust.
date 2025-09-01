c130abcc91c21f9041298eff645d15fd5ce42408
Dipti wagh <diptiwagh305@gmail.com>
2025-09-01 13:27:37 +0530

    Updated filters layout and tax switch position

--- app.js
+++ app.js
@@ -1,114 +1,104 @@
-if (process.env.NODE_ENV != "production") {
-
+if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-//const expressLayouts = require("express-ejs-layouts");
-const ExpressError = require("./utils/ExpressError.js");
-const session = require("express-session");//
+const session = require("express-session");
 const MongoStore = require('connect-mongo');
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
+const ExpressError = require("./utils/ExpressError.js");
 
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const dbUrl = process.env.ATLASDB_URL;
 
-main()
-    .then(() => {
-        console.log("connected to DB");
+// Connect to MongoDB
+mongoose.connect(dbUrl)
+    .then(() => console.log("Connected to DB"))
+    .catch(err => console.log(err));
 
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-async function main() {
-    await mongoose.connect(dbUrl);
-}
+// Set view engine
+app.engine('ejs', ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine('ejs', ejsMate);
-// app.use(expressLayouts);
 app.use(express.static(path.join(__dirname, "/public")));
 
-
+// Session & Flash
 const store = MongoStore.create({
     mongoUrl: dbUrl,
-    crypto: {
-        secret: process.env.SECRET,
-    },
+    crypto: { secret: process.env.SECRET },
     touchAfter: 24 * 3600,
-})
-store.on("error", () => { console.log("ERROR in MONGO SESSION STORE") });
+});
+store.on("error", e => console.log("SESSION STORE ERROR", e));
+
 const sessionOptions = {
     store,
     secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
 };
-//    app.get("/",(req,res)=>{
-//         res.send("Hii i am root");
-//     });
-
-//
-
-
-
 app.use(session(sessionOptions));
 app.use(flash());
+
+// Passport
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
-
-
+// Flash & current user middleware
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     res.locals.currUser = req.user;
     next();
 });
-app.get("/demouser", async (req, res) => {
-    let fakeUser = new User({
-        email: "student@gmail.com",
-        username: "delta student"
-    });
-    let registerUser = await User.register(fakeUser, "hello");
-    res.send(registerUser);
-})
 
+// Routes
+app.get("/", async (req, res) => {
+    const allListings = await require("./models/listing")?.find({});
+    res.render("listings/index", { allListings });
+});
 
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
+// Demo user route (optional)
+app.get("/demouser", async (req, res) => {
+    let fakeUser = new User({ email: "student@gmail.com", username: "delta student" });
+    let registerUser = await User.register(fakeUser, "hello");
+    res.send(registerUser);
+});
 
+// 404 handler
 app.all("*", (req, res, next) => {
-    next(new ExpressError(404, "page not found"));
-})
+    next(new ExpressError(404, "Page not found"));
+});
+
+// Error handler
 app.use((err, req, res, next) => {
-    let { statusCode = 500, message = "Something went wrong" } = err;
-    res.status(statusCode).render("error.ejs", { message });
-    // res.send("something went wrong");
-})
+    const { statusCode = 500, message = "Something went wrong" } = err;
+    res.status(statusCode).render("error", { message });
+});
 
-
-app.listen(8080, () => {
-    console.log("server is listening to port 8080")
+// Start server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
--- routes/listing.js
+++ routes/listing.js
@@ -3,26 +3,27 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
-const listingcontroller = require("../controller/listing.js");
+const listingController = require("../controller/listing.js");
 const multer = require('multer');
 const { storage } = require("../cloudConfig.js");
 const upload = multer({ storage });
-// INDEX & CREATE 
+
+// INDEX & CREATE
 router.route("/")
-    .get(wrapAsync(listingcontroller.index))
+    .get(wrapAsync(listingController.index))
     .post(
         isLoggedIn,
         upload.single("listing[image]"),
         validateListing,
-        wrapAsync(listingcontroller.createlisting)
+        wrapAsync(listingController.createlisting)
     );
-//  NEW FORM 
-router.get("/new", isLoggedIn, listingcontroller.renderNewform);
 
-//  SEARCH 
+// NEW FORM
+router.get("/new", isLoggedIn, listingController.renderNewform);
+
+// SEARCH
 router.get("/search", wrapAsync(async (req, res) => {
-const query = req.query.q ? req.query.q.toString() : ""; 
-// Search multiple fields (title, location, description)
+    const query = req.query.q ? req.query.q.toString() : "";
     const listings = await Listing.find({
         $or: [
             { title: { $regex: query, $options: "i" } },
@@ -30,21 +31,19 @@ const query = req.query.q ? req.query.q.toString() : "";
             { description: { $regex: query, $options: "i" } }
         ]
     });
-
     res.render("listings/index", { allListings: listings });
 }));
 
-//  EDIT 
-router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingcontroller.renderEditform));
+// EDIT
+router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.renderEditform));
 
-//  UPDATE 
-router.put("/:id", isLoggedIn, isOwner, upload.single("image"), validateListing, wrapAsync(listingcontroller.updatelisting));
+// UPDATE
+router.put("/:id", isLoggedIn, isOwner, upload.single("image"), validateListing, wrapAsync(listingController.updatelisting));
 
-//  DELETE 
-router.delete("/:id", isLoggedIn, isOwner, wrapAsync(listingcontroller.destroylisting));
+// DELETE
+router.delete("/:id", isLoggedIn, isOwner, wrapAsync(listingController.destroylisting));
 
-//  SHOW 
-
-router.get("/:id", wrapAsync(listingcontroller.showlisting));
+// SHOW
+router.get("/:id", wrapAsync(listingController.showlisting));
 
 module.exports = router;
